7d58189f2a899926bb11e5c0cc681d91b5daec20
Fábio Souza <fabiobarboza7@hotmail.com>
2019-10-01 00:24:27 -0300

    cleaning controllers

--- app/controllers/api/v1/premises_controller.rb
+++ app/controllers/api/v1/premises_controller.rb
@@ -1,24 +1,17 @@
 class Api::V1::PremisesController < ApplicationController
-	before_action :set_premise, only: [:show, :update, :destroy]
-	before_action :require_authorization!, only: [:show, :update, :destroy]
-
-	# GET /api/v1/premises
 
 	def index
-		@premises = current_user.premises
+		@premises = Premise.all
 		render json: @premises
 	end
 
-	# GET /api/v1/premises/1
-
 	def show
+		@premise = Premise.find(params[:id])
 		render json: @premise
 	end
 
-	# POST /api/v1/premises
-
 	def create
-		@premise = Premise.new(premise_params.merge(user: current_user))
+		@premise = Premise.new(premise_params)
 
 		if @premise.save
 			render json: @premise, status: :created
@@ -28,40 +21,10 @@ class Api::V1::PremisesController < ApplicationController
 
 	end
 
-	# PATCH/PUT /api/v1/premises/1
-
-	def update
-		if @premise.update(premise_params)
-			render json: @premise
-		else
-			render json: @premise.errors, status: :unprocessable_entity
-		end
-	end
-
-	# DELETE /api/v1/premises/1
-
-	def destroy
-		@premise.destroy
-	end
-
 	private
 
-	# Use callbacks to share common setup or constraints between actions.
-
-	def set_premise
-		@premise = Premise.find(params[:id])
-	end
-
-	# Only allow a trusted parameter "white list" through.
-
 	def premise_params
-		params.require(:premise).permit(:name, :email, :phone, :description)
-	end
-
-	def require_authorization!
-		unless current_user == @premise.user
-		render json: {}, status: :forbidden
-		end
+		params.require(:premise).permit(:name, :address)
 	end
 
 end
--- app/controllers/api/v1/stations_controller.rb
+++ app/controllers/api/v1/stations_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::StationsController < ApplicationController
+
+	def index
+		@stations = Station.all
+		render json: @stations
+	end
+
+	def create
+		@station = Station.new(premise_params)
+
+		if @station.save
+			render json: @station, status: :created
+		else
+			render json: @station.errors, status: :unprocessable_entity
+		end
+
+	end
+
+	private
+
+	def premise_params
+		params.require(:premise).permit(:serial, :name, :premise_id)
+	end
+
 end
