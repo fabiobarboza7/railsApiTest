fa55c4f3ec0479bfa84d77ad68a0253038b4422d
Fábio Souza <fabiobarboza7@hotmail.com>
2019-10-03 01:33:31 -0300

    finishing test

--- spec/requests/get_premises_spec.rb
+++ spec/requests/get_premises_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-describe "get all questions route", :type => :request do
-  let!(:premises) {FactoryBot.create_list(:random_premise, 20)}
+describe "get all premises route", :type => :request do
+  let!(:premises) {FactoryBot.create_list(:random_premise, 5)}
 	before {get '/api/v1/premises'}
 
 	it 'returns all premises' do
-    expect(JSON.parse(response.body).size).to eq(20)
+    expect(JSON.parse(response.body).size).to eq(5)
   end
 
 	it 'returns status code 200' do
